e396f29689aba3d04eb1f8c1c11466623f19c0cd
Kamil Kolanowski <tanssive@yahoo.com>
2025-06-28 17:24:51 +0200

    create box for crud shopping list

--- shopping-list-web/src/pages/Home.jsx
+++ shopping-list-web/src/pages/Home.jsx
@@ -1,5 +1,3 @@
-
-
 function Home() {
     return (
         <div className="home">
@@ -9,9 +7,41 @@ function Home() {
                 <div className="menu">Delete Shopping List</div>
                 <div className="menu">View Shopping List</div>
             </menu>
-
             <div className="main-view">
+                <section className="main-section">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sit amet lectus at facilisis.
+                        Nullam at sapien lorem. Pellentesque in sollicitudin urna. Nunc suscipit nulla ultrices urna
+                        euismod, rutrum varius lectus hendrerit. Praesent aliquet mollis diam, non vehicula eros congue
+                        at. Morbi vel commodo libero. Fusce dignissim lacus at porta laoreet. Suspendisse id urna
+                        tellus. Vivamus malesuada arcu quis semper ornare. Maecenas nibh risus, tempor in convallis at,
+                        rutrum quis risus. Etiam molestie lorem eu elit malesuada posuere at semper mauris. Quisque
+                        molestie nec leo sit amet convallis. Fusce eu metus vitae risus fringilla placerat in eget
+                        mauris. Nunc convallis, magna et consequat fermentum, mauris enim sollicitudin massa, eget
+                        volutpat neque turpis in diam.</p>
+                    <br/>
+                    <p>Duis ac nunc cursus, placerat dolor sed, ultricies mi. Suspendisse consectetur justo neque,
+                        placerat congue ante fringilla et. Suspendisse in imperdiet elit, quis ullamcorper sapien.
+                        Vivamus vel hendrerit nunc. Fusce vel leo sed magna imperdiet pretium. Donec ut neque tristique,
+                        pharetra ex eu, sollicitudin mauris. Praesent elementum est et varius rutrum.</p>
+                    <br/>
+                    <p>In id nisi eu lectus molestie pellentesque eget euismod purus. Vivamus eget dui at diam dictum
+                        condimentum et sed lorem. Mauris nulla nibh, aliquet ac rhoncus porttitor, mollis at felis.
+                        Nullam cursus finibus orci, ac laoreet ex molestie sed. Aenean cursus sem vitae massa fermentum
+                        facilisis. Nullam ligula risus, consectetur et placerat et, interdum in nibh. Ut id elementum
+                        mauris.</p>
 
+                    <p>Donec vel hendrerit mauris. Maecenas interdum volutpat mi, nec eleifend erat tincidunt quis.
+                        Etiam imperdiet mi enim, et lacinia arcu euismod sed. Nam aliquet, sapien eu dictum tincidunt,
+                        erat lectus tristique justo, sed viverra metus purus et arcu. In hac habitasse platea dictumst.
+                        Donec consectetur leo dui, nec congue felis accumsan quis. Etiam nisi diam, aliquam ac lobortis
+                        vel, maximus eget enim. Maecenas suscipit diam at libero cursus, id malesuada quam bibendum.
+                        Curabitur dignissim ante vel odio auctor tempor. Ut aliquet nisl dapibus enim volutpat
+                        condimentum. Pellentesque quis ipsum ante. Orci varius natoque penatibus et magnis dis
+                        parturient montes, nascetur ridiculus mus. Morbi nec est consequat, ullamcorper arcu at,
+                        vestibulum massa. Cras ac facilisis odio. Ut scelerisque orci ac ipsum commodo fermentum.</p>
+
+
+                </section>
             </div>
         </div>
     );
